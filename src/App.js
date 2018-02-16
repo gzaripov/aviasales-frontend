64f240e3b844219693a68f1ba81e9974b2bff615
Grigory Zaripov <grigory.zaripov@avaintec.com>
2018-02-17 00:00:56 +0300

    feat: added promotion component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Header from "./Header";
 import Destinations from "./Destinations";
 import Prices from "./Prices";
+import Promotion from "./Promotion";
 import Offers from "./Offers";
 import Articles from "./Articles";
 import MobileApp from "./MobileApp";
@@ -14,6 +15,7 @@ class App extends Component {
         <Header />
         <Destinations />
         <Prices />
+        <Promotion />
         <Offers />
         <Articles />
         <MobileApp />
