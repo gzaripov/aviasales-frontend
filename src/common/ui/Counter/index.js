592af5f5080f71e79d6e7548394a1e6ac4f21e64
Grigory Zaripov <grigory.zaripov@avaintec.com>
2018-03-10 11:56:17 +0300

    style: change counter increment/decrement borders

--- src/common/ui/Counter/index.js
+++ src/common/ui/Counter/index.js
@@ -22,8 +22,7 @@ const Increment = styled.button`
   border: none;
   cursor: pointer;
   border: 1px solid #dbdbdb;
-  border-top-right-radius: 4px;
-  border-bottom-right-radius: 4px;
+  border-radius: 0 4px 4px 0;
   transition: border-color 0.3s, color 0.3s;
   &:hover {
     color: #00b8e9;
@@ -40,7 +39,7 @@ const Increment = styled.button`
 `;
 
 const Decrement = Increment.extend`
-  border: 4px 0 0 4px;
+  border-radius: 4px 0 0 4px;
 `;
 
 const IncrementIcon = styled.img`
